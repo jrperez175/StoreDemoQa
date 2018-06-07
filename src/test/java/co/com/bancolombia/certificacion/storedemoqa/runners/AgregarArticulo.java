59e465042cf33a66e782f68b16119a658bd97845
Jhon Perez <jrperez175@gmaill.com>
2018-06-07 16:21:56 -0500

    Commit 07/junio/2018 Version final 1.0

--- src/test/java/co/com/bancolombia/certificacion/storedemoqa/runners/AgregarArticulo.java
+++ src/test/java/co/com/bancolombia/certificacion/storedemoqa/runners/AgregarArticulo.java
@@ -6,6 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 
+
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = {"src/test/resources/features/agregar_articulo.feature"},
 				glue = {"co.com.bancolombia.certificacion.storedemoqa.stepdefinitions"},
